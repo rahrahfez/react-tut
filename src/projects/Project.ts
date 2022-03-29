45ade4ff3a272327a9bf37b245a3909385538732
david om <rahrahfez@hotmail.com>
2022-03-28 22:48:42 -0400

    initial commit

---
+++ src/projects/Project.ts
@@ -0,0 +1,28 @@
+export class Project {
+  id: number | undefined;
+  name = '';
+  description = '';
+  imageUrl = '';
+  contractTypeId: number | undefined;
+  contractSignedOn: Date = new Date();
+  budget = 0;
+  isActive = false;
+  get isNew(): boolean {
+    return this.id === undefined;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(initializer?: any) {
+    if (!initializer) return;
+    if (initializer.id) this.id = initializer.id;
+    if (initializer.name) this.name = initializer.name;
+    if (initializer.description) this.description = initializer.description;
+    if (initializer.imageUrl) this.imageUrl = initializer.imageUrl;
+    if (initializer.contractTypeId)
+      this.contractTypeId = initializer.contractTypeId;
+    if (initializer.contractSignedOn)
+      this.contractSignedOn = initializer.contractSignedOn;
+    if (initializer.budget) this.budget = initializer.budget;
+    if (initializer.isActive) this.isActive = initializer.isActive;
+  }
+}
